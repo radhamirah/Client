021186481b658e6c85686f03ba4dc3e156b9c3dc
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-19 14:17:17 +0800

    Update tcpclient.c

--- tcpclient.c
+++ tcpclient.c
@@ -12,7 +12,7 @@ int main()
   struct sockaddr_in server_address ;
   server_address.sin_family=AF_INET ;
   server_address.sin_port=htons(9002) ;
-  server_address.sin_addr.s_addr=inet_addr("192.168.31.128") ;
+  server_address.sin_addr.s_addr=inet_addr("192.168.31.134") ;
   
   int stat=connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
   if (stat== -1);

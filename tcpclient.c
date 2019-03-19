83c16247b783d847454822c747c5791d22aa2318
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-19 14:00:44 +0800

    Update tcpclient.c

--- tcpclient.c
+++ tcpclient.c
@@ -14,7 +14,7 @@ int main()
   struct sockaddr_in server_address ;
   server_address.sin_family = AF_INET ;
   server_address.sin_port = htons(9002) ;
-  server_address.sin_addr.s_addr = inet_addr("127.0.0.1") ;
+  server_address.sin_addr.s_addr = inet_addr("192.168.31.128") ;
   
   int stat = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
   //check for error with connection

d4ad6b248ba36631b92592c2ecb3d702e65c65de
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-19 12:38:21 +0800

    Update .c

--- .c
+++ .c
@@ -13,7 +13,7 @@ int main()
   //specify an address for the socket
   struct sockaddr_in server_address ;
   server_address.sin_family = AF_INET ;
-  server_address.sin_port = htons(9002) ;
+  server_address.sin_port = htons(9876) ;
   server_address.sin_addr.s_addr = inet_addr("127.0.0.1") ;
   
   int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));

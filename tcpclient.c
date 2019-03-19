c56e5b3d1d0b1f1d5fd5b25a4790e2de640581b6
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-19 15:06:26 +0800

    Update tcpclient.c

--- tcpclient.c
+++ tcpclient.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 
 int main() 
 {
